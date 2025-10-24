a3a654e3defa42e1ad15df0c05c969eca524dffd
huije08 <huijegang@gmail.com>
2025-10-24 19:45:58 +0900

    (2025.10.24) 우선순위 큐

---
+++ Class6th (Linear Queue)/Class6th (Linear Queue).cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+
+using namespace std;
+template <typename T>
+
+#define SIZE 5
+
+class Queue
+{
+private:
+	int rear;
+	int front;
+
+	T container[SIZE];
+public:
+	Queue()
+	{
+		rear = 0;
+		front = 0;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			container[i] = NULL;
+		}
+	}
+
+	void push(T data)
+	{
+		if (rear >= SIZE)
+		{
+			cout << "linear queue overflow" << endl;
+
+		}
+		else
+		{
+			container[rear++] = data;
+		}
+	}
+
+	void pop()
+	{
+		if (empty())
+		{
+			cout << "linear queue is empty" << endl;
+		}
+		else
+		{
+			container[front++] = data;
+		}
+	}
+
+	const bool& empty()
+	{
+		return front == rear;
+	}
+
+	const T& peak()
+	{
+		if (empty())
+		{
+			exit(i);
+		}
+		else
+		{
+			return container[front];
+		}
+	}
+
+	const int& size()
+	{
+		return rear - front;
+	}
+};
+
+
+int main()
+{
+	Queue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+	queue.push(40);
+	queue.push(50);
+
+
+	while (queue.empty() == false)
+	{
+		cout << queue.peak() << endl;
+	}
+	cout << "Queue size = " << queue.size() << endl;
+
+	queue.push(60);
+	return 0;
+
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -3,20 +3,22 @@
 using namespace std;
 template <typename T>
 
-#define SIZE 5
+#define SIZE 4
 
 class Queue
 {
 private:
+
 	int rear;
 	int front;
 
 	T container[SIZE];
+
 public:
 	Queue()
 	{
-		rear = 0;
-		front = 0;
+		rear = SIZE - 1;
+		front = SIZE - 1;
 
 		for (int i = 0; i < SIZE; i++)
 		{
@@ -26,26 +28,63 @@ public:
 
 	void push(T data)
 	{
-		if (true)
+		if (front == (rear + 1)%SIZE)
 		{
-
+			cout << "linear queue overflow" << endl;
+		}
+		else
+		{
+			rear == (rear + 1) % SIZE;
+			container[rear] = data;
 		}
 	}
 
 	void pop()
 	{
-	
+		if (empty())
+		{
+			cout << "linear queue is empty" << endl;
+		}
+		else
+		{
+			rear == (rear + 1) % SIZE;
+			container[rear] = data;
+		}
 	}
 
 	const bool& empty()
 	{
+		return front == rear;
+	}
 
+	const T& peak()
+	{
+		if (empty())
+		{
+			exit(i);
+		}
+		else
+		{
+			return container[];
+		}
 	}
 };
 
 
+
 int main()
 {
+	Queue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+
+	while (queue.empty() == false)
+	{
+
+	}
+
 
 	return 0;
 
